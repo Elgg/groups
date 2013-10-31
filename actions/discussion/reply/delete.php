Elgg/Elgg

Updated the basic CRUD features to treat discussion replies as entities.

mod/groups/actions/discussion/reply/delete.php
@@ -3,10 +3,11 @@
  * Delete discussion reply
  */
 
-$id = (int) get_input('annotation_id');
+$guid = (int) get_input('guid');
 
-$reply = elgg_get_annotation_from_id($id);
-if (!$reply || $reply->name != 'group_topic_post') {
+$reply = get_entity($guid);
+
+if (!elgg_instanceof($reply, 'object', 'groupforumreply')) {
 	register_error(elgg_echo('discussion:reply:error:notdeleted'));
 	forward(REFERER);
 }
@@ -16,8 +17,7 @@ if (!$reply->canEdit()) {
 	forward(REFERER);
 }
 
-$result = $reply->delete();
-if ($result) {
+if ($reply->delete()) {
 	system_message(elgg_echo('discussion:reply:deleted'));
 } else {
 	register_error(elgg_echo('discussion:reply:error:notdeleted'));

mod/groups/views/default/object/groupforumtopic.php
@@ -29,21 +29,29 @@ $date = elgg_view_friendly_time($topic->time_created);
 
 $replies_link = '';
 $reply_text = '';
-$num_replies = elgg_get_annotations(array(
-	'annotation_name' => 'group_topic_post',
-	'guid' => $topic->getGUID(),
+
+$num_replies = elgg_get_entities(array(
+	'type' => 'object',
+	'subtype' => 'groupforumreply',
+	'container_guid' => $topic->getGUID(),
 	'count' => true,
 ));
+
 if ($num_replies != 0) {
-	$last_reply = $topic->getAnnotations(array(
-		'annotation_name' => 'group_topic_post',
+	$last_reply = elgg_get_entities(array(
+		'type' => 'object',
+		'subtype' => 'groupforumreply',
+		'container_guid' => $topic->getGUID(),
 		'limit' => 1,
-		'reverse_order_by' => true,
 	));
-	$poster = $last_reply[0]->getOwnerEntity();
-	$reply_time = elgg_view_friendly_time($last_reply[0]->time_created);
+	if (isset($last_reply[0])) {
+		$last_reply = $last_reply[0];
+	}
+
+	$poster = $last_reply->getOwnerEntity();
+	$reply_time = elgg_view_friendly_time($last_reply->time_created);
 	$reply_text = elgg_echo('groups:updated', array($poster->name, $reply_time));
-	
+
 	$replies_link = elgg_view('output/url', array(
 		'href' => $topic->getURL() . '#group-replies',
 		'text' => elgg_echo('group:replies') . " ($num_replies)",
@@ -51,16 +59,16 @@ if ($num_replies != 0) {
 	));
 }
 
-$metadata = elgg_view_menu('entity', array(
-	'entity' => $vars['entity'],
-	'handler' => 'discussion',
-	'sort_by' => 'priority',
-	'class' => 'elgg-menu-hz',
-));
-
 // do not show the metadata and controls in widget view
 if (elgg_in_context('widgets')) {
 	$metadata = '';
+} else {
+	$metadata = elgg_view_menu('entity', array(
+		'entity' => $vars['entity'],
+		'handler' => 'discussion',
+		'sort_by' => 'priority',
+		'class' => 'elgg-menu-hz',
+	));
 }
 
 if ($full) {

Elgg/groups

actions/discussion/reply/delete.php
@@ -3,10 +3,11 @@
  * Delete discussion reply
  */
 
-$id = (int) get_input('annotation_id');
+$guid = (int) get_input('guid');
 
-$reply = elgg_get_annotation_from_id($id);
-if (!$reply || $reply->name != 'group_topic_post') {
+$reply = get_entity($guid);
+
+if (!elgg_instanceof($reply, 'object', 'discussion_reply')) {
 	register_error(elgg_echo('discussion:reply:error:notdeleted'));
 	forward(REFERER);
 }
@@ -16,8 +17,7 @@ if (!$reply->canEdit()) {
 	forward(REFERER);
 }
 
-$result = $reply->delete();
-if ($result) {
+if ($reply->delete()) {
 	system_message(elgg_echo('discussion:reply:deleted'));
 } else {
 	register_error(elgg_echo('discussion:reply:error:notdeleted'));

views/default/object/groupforumtopic.php
@@ -29,21 +29,29 @@ $date = elgg_view_friendly_time($topic->time_created);
 
 $replies_link = '';
 $reply_text = '';
-$num_replies = elgg_get_annotations(array(
-	'annotation_name' => 'group_topic_post',
-	'guid' => $topic->getGUID(),
+
+$num_replies = elgg_get_entities(array(
+	'type' => 'object',
+	'subtype' => 'discussion_reply',
+	'container_guid' => $topic->getGUID(),
 	'count' => true,
 ));
+
 if ($num_replies != 0) {
-	$last_reply = $topic->getAnnotations(array(
-		'annotation_name' => 'group_topic_post',
+	$last_reply = elgg_get_entities(array(
+		'type' => 'object',
+		'subtype' => 'discussion_reply',
+		'container_guid' => $topic->getGUID(),
 		'limit' => 1,
-		'reverse_order_by' => true,
 	));
-	$poster = $last_reply[0]->getOwnerEntity();
-	$reply_time = elgg_view_friendly_time($last_reply[0]->time_created);
+	if (isset($last_reply[0])) {
+		$last_reply = $last_reply[0];
+	}
+
+	$poster = $last_reply->getOwnerEntity();
+	$reply_time = elgg_view_friendly_time($last_reply->time_created);
 	$reply_text = elgg_echo('groups:updated', array($poster->name, $reply_time));
-	
+
 	$replies_link = elgg_view('output/url', array(
 		'href' => $topic->getURL() . '#group-replies',
 		'text' => elgg_echo('group:replies') . " ($num_replies)",
@@ -51,16 +59,16 @@ if ($num_replies != 0) {
 	));
 }
 
-$metadata = elgg_view_menu('entity', array(
-	'entity' => $vars['entity'],
-	'handler' => 'discussion',
-	'sort_by' => 'priority',
-	'class' => 'elgg-menu-hz',
-));
-
 // do not show the metadata and controls in widget view
 if (elgg_in_context('widgets')) {
 	$metadata = '';
+} else {
+	$metadata = elgg_view_menu('entity', array(
+		'entity' => $vars['entity'],
+		'handler' => 'discussion',
+		'sort_by' => 'priority',
+		'class' => 'elgg-menu-hz',
+	));
 }
 
 if ($full) {